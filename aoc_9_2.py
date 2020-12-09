e1a2950d3da13fbcfc3d5c86b46c30ff38f25d98
Showherda <showherda@gmail.com>
2020-12-09 16:08:27 +0600

    Refactored down to O(n)

--- aoc_9_2.py
+++ aoc_9_2.py
@@ -1,12 +1,18 @@
 import sys
 sys.stdin=open('input.txt')
-num=[int(v) for v in sys.stdin.readlines()]
+inp=[int(v) for v in sys.stdin.readlines()]
+n=len(inp)
 val=542529149
-n=len(num)
-inp=num.copy()
+num=inp.copy()
 for i in range(1, n):
-	inp[i]+=inp[i-1]
-for i in range(n):
-	for j in range(n):
-		if i-j>1 and inp[i]-inp[j]==val:
-			print(min(num[j+1:i+1])+max(num[j+1:i+1]))
+	num[i]+=num[i-1]
+p1=0
+while p1<n:
+	p2=p1
+	while num[p2]-num[p1]<val:
+		p2+=1
+	if p2-p1>1 and num[p2]-num[p1]==val:
+		print(min(inp[p1+1:p2+1])+max(inp[p1+1:p2+1]))
+		break
+	while p2>=p1 and num[p2]-num[p1]>val:
+		p1+=1
