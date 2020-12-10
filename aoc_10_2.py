a7c1f941192797bd293fac18ad16c07f6f6bb9d6
Showherda <showherda@gmail.com>
2020-12-10 12:51:08 +0600

    Update and rename aoc.py to aoc_10_2.py

--- aoc_10_2.py
+++ aoc_10_2.py
@@ -1,12 +1,5 @@
 import sys
-# LI=lambda:list(map(int, sys.stdin.readline().split()))
-# MI=lambda:map(int, sys.stdin.readline().split())
-# SI=lambda:sys.stdin.readline().strip('\n')
-# II=lambda:int(sys.stdin.readline())
 sys.stdin=open('input.txt')
-#sys.stdout=open('output.txt', 'w')
-# inp=sorted([int(v) for v in sys.stdin.readlines()])
-# n=len(inp)
 inp=[int(v) for v in sys.stdin.readlines()]
 m=max(inp)
 tab=set(inp)
@@ -27,5 +20,4 @@ def fun(u):
 		memo[u]+=fun(u+3)
 	done.add(u)
 	return memo[u]
-# print(inp)
 print(fun(0))
