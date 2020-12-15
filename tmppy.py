592a35119ed63ba6c3b20172fb7f0772075768c7
Showherda <showherda@gmail.com>
2020-12-15 19:15:05 +0600

    Add files via upload

---
+++ tmppy.py
@@ -0,0 +1,21 @@
+import sys
+inp=[1, 0, 15, 2, 10, 13]
+mem={}
+val=2020	# put in 30000000 for part 2
+for i, v in enumerate(inp, 1):
+	mem[v]=[i]
+while len(inp)<val:
+	num=inp[-1]
+	if len(mem[num])==1:
+		inp.append(0)
+		try:
+			mem[0].append(len(inp))
+		except:
+			mem[0]=[len(inp)]
+	else:
+		inp.append(mem[num][-1]-mem[num][-2])
+		try:
+			mem[inp[-1]].append(len(inp))
+		except:
+			mem[inp[-1]]=[len(inp)]
+print(inp[val-1])
