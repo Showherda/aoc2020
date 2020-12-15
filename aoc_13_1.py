f0ac3866f6442f7c8146a07dc6e9a5a6cfff7cc3
Showherda <showherda@gmail.com>
2020-12-15 19:56:21 +0600

    Update and rename tmppy.py to aoc_13_1.py

--- aoc_13_1.py
+++ aoc_13_1.py
@@ -5,10 +5,6 @@ inp=[]
 for v in sys.stdin.readline().split(','):
 	if v!='x':
 		inp.append(int(v))
-# inp=set(v.strip('\n') for v in sys.stdin.readline().split(','))
-# inp.remove('x')
-# inp=[int(v) for v in inp]
-# print(inp)
 from math import ceil
 d=10000000000
 for v in inp:
